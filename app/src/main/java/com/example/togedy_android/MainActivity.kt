675c119d2f1946757556c7dabd2379ca306ccb13
chrin05 <rinring105@gmail.com>
2024-10-10 02:46:25 +0900

    feat : NavigationBar 적용

--- app/src/main/java/com/example/togedy_android/MainActivity.kt
+++ app/src/main/java/com/example/togedy_android/MainActivity.kt
@@ -3,13 +3,31 @@ package com.example.togedy_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.togedy_android.ui.bottom_nav.BottomNavigationBar
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.togedy_android.ui.bottom_nav.BottomNavigationItem
+import com.example.togedy_android.ui.model.NavGraph
+import com.example.togedy_android.ui.theme.Pretendard
 import com.example.togedy_android.ui.theme.Togedy_AndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,22 +36,85 @@ class MainActivity : ComponentActivity() {
         setContent {
             Togedy_AndroidTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    BottomNavigationBar()
+//                    BottomNavigationBar()
+                    val navController = rememberNavController()
+                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+                    val currentRoute = navBackStackEntry?.destination?.route
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        bottomBar = {
+                            if (currentRoute !in listOf(
+                                    "launchPage",
+                                    "signIn",
+                                    "joinMembership",
+                                    "ResignMemberShip"
+                                )
+                            ) {
+                                NavigationBar(
+                                    containerColor = colorResource(id = R.color.white),
+                                    tonalElevation = 10.dp
+                                ) {
+                                    BottomNavigationItem().bottomNavigationItems()
+                                        .forEachIndexed { _, item ->
+                                            NavigationBarItem(
+                                                selected = item.route == currentRoute,
+                                                label = {
+                                                    Text(
+                                                        text = item.label,
+                                                        fontFamily = Pretendard,
+                                                        fontWeight = FontWeight.Medium,
+                                                        fontSize = 12.sp
+                                                    )
+                                                },
+                                                onClick = {
+                                                    navController.navigate(item.route) {
+                                                        popUpTo(navController.graph.findStartDestination().id) {
+                                                            saveState = true
+                                                        }
+                                                        launchSingleTop = true
+                                                        restoreState = true
+                                                    }
+                                                },
+                                                icon = {
+                                                    Icon(
+                                                        painterResource(id = item.icon),
+                                                        contentDescription = item.label
+                                                    )
+                                                },
+                                                colors = NavigationBarItemDefaults.colors(
+                                                    selectedIconColor = colorResource(id = R.color.yellow_main),
+                                                    unselectedIconColor = colorResource(id = R.color.gray400),
+                                                    selectedTextColor = colorResource(id = R.color.yellow_main),
+                                                    unselectedTextColor = colorResource(id = R.color.gray400),
+                                                    indicatorColor = Color.Transparent
+                                                )
+                                            )
+                                        }
+                                }
+                            }
+                        }
+                    ) {
+                        Box(
+                            modifier = Modifier.padding(it)
+                        ) {
+                            NavGraph(navController)
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting() {
-    BottomNavigationBar()
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Togedy_AndroidTheme {
-        Greeting()
-    }
-}
+//@Composable
+//fun Greeting() {
+//    BottomNavigationBar()
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    Togedy_AndroidTheme {
+//        Greeting()
+//    }
+//}
