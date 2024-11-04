10f92592c3bb5fbbd65dde55815a3268c266db69
chrin05 <rinring105@gmail.com>
2024-11-04 20:52:50 +0900

    #3 fix: Theme 수정

--- app/src/main/java/com/example/togedy_android/ui/theme/Theme.kt
+++ app/src/main/java/com/example/togedy_android/ui/theme/Theme.kt
@@ -1,26 +1,28 @@
 package com.example.togedy_android.ui.theme
 
 import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = yellowMain,
+    secondary = gray600,
+    tertiary = yellow200
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = yellowMain,
+    secondary = gray600,
+    tertiary = yellow200
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -33,26 +35,75 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+object TogedyTheme {
+    val colors: TogedyColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTogedyColors.current
+
+    val typography: TogedyTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTogedyTypography.current
+}
+
 @Composable
-fun Togedy_AndroidTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+fun ProvideDateRoadColorsAndTypography(
+    colors: TogedyColors,
+    typography: TogedyTypography,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
+    CompositionLocalProvider(
+        LocalTogedyColors provides colors,
+        LocalTogedyTypography provides typography,
         content = content
     )
 }
+
+@Composable
+fun Togedy_AndroidTheme(
+    backgroundColor: Color = defaultTogedyColors.white,
+    content: @Composable () -> Unit
+) {
+    ProvideDateRoadColorsAndTypography(
+        colors = defaultTogedyColors,
+        typography = defaultTogedyTypography
+    ) {
+        val view = LocalView.current
+        if (!view.isInEditMode) {
+            SideEffect {
+                (view.context as Activity).window.run {
+                    statusBarColor = backgroundColor.toArgb()
+                    WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars = true
+                }
+            }
+        }
+
+        MaterialTheme(
+            content = content
+        )
+    }
+}
+//@Composable
+//fun Togedy_AndroidTheme(
+//    darkTheme: Boolean = isSystemInDarkTheme(),
+//    // Dynamic color is available on Android 12+
+//    dynamicColor: Boolean = true,
+//    content: @Composable () -> Unit
+//) {
+//    val colorScheme = when {
+////        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+////            val context = LocalContext.current
+////            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+////        }
+////
+////        darkTheme -> DarkColorScheme
+////        else -> LightColorScheme
+////    }
+//
+//    MaterialTheme(
+//        colorScheme = colorScheme,
+//        typography = Typography,
+//        content = content
+//    )
+//}
