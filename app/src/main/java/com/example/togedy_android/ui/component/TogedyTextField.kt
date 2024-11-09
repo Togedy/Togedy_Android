c1733fbff70ccbdafd4f6658d400f47315441cf4
chrin05 <rinring105@gmail.com>
2024-11-10 01:12:19 +0900

    #5 feat: 지우기 버튼 추가

--- app/src/main/java/com/example/togedy_android/ui/component/TogedyTextField.kt
+++ app/src/main/java/com/example/togedy_android/ui/component/TogedyTextField.kt
@@ -1,6 +1,8 @@
 package com.example.togedy_android.ui.component
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,21 +10,26 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.room.util.copy
+import com.example.togedy_android.R
 import com.example.togedy_android.ui.theme.TogedyTheme
 
 @Composable
@@ -52,26 +59,43 @@ fun BorderTextField(
         )
         Spacer(modifier = Modifier.height(4.dp))
 
-        BasicTextField(
-            value = value,
-            onValueChange = onTextFieldChange,
-            textStyle = textStyle.copy(
-                color = borderColor,
-                textAlign = TextAlign.Start
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(44.dp)
-                .border(
-                    width = 1.dp,
-                    color = borderColor,
-                    shape = RoundedCornerShape(8.dp)
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.CenterEnd
+        ) {
+            BasicTextField(
+                value = value,
+                onValueChange = onTextFieldChange,
+                textStyle = textStyle.copy(
+                    color = TogedyTheme.colors.black,
+                    textAlign = TextAlign.Start
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(44.dp)
+                    .border(
+                        width = 1.dp,
+                        color = borderColor,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(10.dp)
+                    .onFocusEvent { onFocus = it.isFocused },
+                visualTransformation = visualTransformation,
+                singleLine = isSingleLine,
+            )
+            if (onFocus && value.isNotEmpty()) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_x_circle),
+                    contentDescription = "모두 지우기",
+                    modifier = Modifier
+                        .padding(end = 8.dp)
+                        .clickable {
+                            onTextFieldChange("")
+                        }
                 )
-                .padding(10.dp)
-                .onFocusEvent { onFocus = it.isFocused },
-            visualTransformation = visualTransformation,
-            singleLine = isSingleLine,
-        )
+            }
+        }
+
     }
 }
 
