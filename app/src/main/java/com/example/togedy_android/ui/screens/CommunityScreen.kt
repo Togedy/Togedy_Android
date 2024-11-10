93771d17a53b6f2e11df1d3dd8102638ba32406a
chrin05 <rinring105@gmail.com>
2024-11-10 20:22:04 +0900

    #7 feat: 실시간 인기글 섹션 구현

--- app/src/main/java/com/example/togedy_android/ui/screens/CommunityScreen.kt
+++ app/src/main/java/com/example/togedy_android/ui/screens/CommunityScreen.kt
@@ -1,29 +1,17 @@
 package com.example.togedy_android.ui.screens
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.togedy_android.R
 import com.example.togedy_android.ui.component.community.CommunityFastTab
 import com.example.togedy_android.ui.component.community.CommunityHomeTopBar
+import com.example.togedy_android.ui.component.community.TrendingPostsSection
 import com.example.togedy_android.ui.theme.TogedyTheme
 import com.example.togedy_android.ui.theme.Togedy_AndroidTheme
 
@@ -32,7 +20,7 @@ fun CommunityScreen() {
     Togedy_AndroidTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
-            color = colorScheme.background
+            color = TogedyTheme.colors.white
         ) {
             Column(
                 modifier = Modifier
@@ -48,7 +36,10 @@ fun CommunityScreen() {
                     onMarketplaceClicked = { }
                 )
 
+                Spacer(modifier = Modifier.height(30.dp))
+                TrendingPostsSection()
 
+                Spacer(modifier = Modifier.height(30.dp))
             }
         }
     }
