de7c6847ce2e324cfff3f15cec27f0998be36f6d
chrin05 <rinring105@gmail.com>
2024-11-10 19:58:31 +0900

    #7 feat: 커뮤니티 메인화면 구현 중

--- app/src/main/java/com/example/togedy_android/ui/screens/CommunityScreen.kt
+++ app/src/main/java/com/example/togedy_android/ui/screens/CommunityScreen.kt
@@ -1,26 +1,30 @@
 package com.example.togedy_android.ui.screens
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.example.togedy_android.R
+import com.example.togedy_android.ui.component.community.CommunityFastTab
+import com.example.togedy_android.ui.component.community.CommunityHomeTopBar
+import com.example.togedy_android.ui.theme.TogedyTheme
 import com.example.togedy_android.ui.theme.Togedy_AndroidTheme
 
 @Composable
@@ -31,29 +35,27 @@ fun CommunityScreen() {
             color = colorScheme.background
         ) {
             Column(
-                modifier = Modifier.fillMaxSize().padding(15.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                modifier = Modifier
+                    .fillMaxSize()
             ) {
-                Box(
-                    modifier = Modifier.fillMaxWidth()
-                        .height(200.dp)
-                        .padding(horizontal = 15.dp, vertical = 10.dp)
-                        .clip(MaterialTheme.shapes.large)
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.ic_home),
-                        contentDescription = "home_screen_bg",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
-                Text(
-                    "Home Screen",
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.padding(vertical = 20.dp)
+                CommunityHomeTopBar(
+                    onMenuBtnClicked = { }
                 )
+
+                CommunityFastTab(
+                    onStudyRecordItemClicked = { },
+                    onCollegeItemClicked = { },
+                    onMarketplaceClicked = { }
+                )
+
+
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun CommunityScreenPreview(modifier: Modifier = Modifier) {
+    CommunityScreen()
 }
