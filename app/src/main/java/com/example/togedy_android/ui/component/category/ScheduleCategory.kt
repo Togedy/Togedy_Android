97e20cae2e11c0c9061aaae261dc92b0742ec7ee
chrin05 <rinring105@gmail.com>
2024-11-10 19:06:55 +0900

    fix: 불필요한 import문 삭제

--- app/src/main/java/com/example/togedy_android/ui/component/category/ScheduleCategory.kt
+++ app/src/main/java/com/example/togedy_android/ui/component/category/ScheduleCategory.kt
@@ -18,10 +18,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
--- app/src/main/java/com/example/togedy_android/ui/screens/calendar/AddPersonalSchedule.kt
+++ app/src/main/java/com/example/togedy_android/ui/screens/calendar/AddPersonalSchedule.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
