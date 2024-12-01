5624a26da7b8be2456072e1430e0a14d221c0877
chrin05 <rinring105@gmail.com>
2024-12-01 23:38:10 +0900

    feat/#14: gpt 네비게이션 구현

---
+++ app/src/main/java/com/example/togedy_android/presentation/ui/screens/gptscreen/navigation/GptNavigation.kt
@@ -0,0 +1,22 @@
+package com.example.togedy_android.presentation.ui.screens.gptscreen.navigation
+
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.example.togedy_android.core.navigation.MainTabRoute
+
+fun NavHostController.navigateToGPT(navOptions: NavOptions? = null) = navigate(Gpt)
+
+fun NavGraphBuilder.gptScreen(
+    navController: NavController,
+    modifier: Modifier = Modifier,
+) {
+    composable<Gpt> {
+
+    }
+}
+
+data object Gpt: MainTabRoute
