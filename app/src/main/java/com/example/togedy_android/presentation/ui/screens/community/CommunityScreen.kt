11e7647fe1e72a024a2583d14f422a78414c5856
chrin05 <rinring105@gmail.com>
2024-12-01 20:06:49 +0900

    ui/#7: 커뮤니티 홈 UI 구현

--- app/src/main/java/com/example/togedy_android/presentation/ui/screens/community/CommunityScreen.kt
+++ app/src/main/java/com/example/togedy_android/presentation/ui/screens/community/CommunityScreen.kt
@@ -1,59 +1,60 @@
 package com.example.togedy_android.presentation.ui.screens.community
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import com.example.togedy_android.R
+import com.example.togedy_android.presentation.ui.screens.community.component.CommunityFastTab
+import com.example.togedy_android.presentation.ui.screens.community.component.CommunityHomeTopBar
+import com.example.togedy_android.presentation.ui.screens.community.component.HomeBulletinBoard
+import com.example.togedy_android.presentation.ui.screens.community.component.TrendingPostsSection
+import com.example.togedy_android.ui.theme.TogedyTheme
 import com.example.togedy_android.ui.theme.Togedy_AndroidTheme
 
 @Composable
-fun CommunityScreen(navController: NavController) {
+fun CommunityScreen(
+    onMenuBtnClicked: () -> Unit,
+) {
     Togedy_AndroidTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
-            color = colorScheme.background
+            color = TogedyTheme.colors.white
         ) {
             Column(
-                modifier = Modifier.fillMaxSize().padding(15.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                modifier = Modifier
+                    .fillMaxSize()
             ) {
-                Box(
-                    modifier = Modifier.fillMaxWidth()
-                        .height(200.dp)
-                        .padding(horizontal = 15.dp, vertical = 10.dp)
-                        .clip(MaterialTheme.shapes.large)
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.ic_home),
-                        contentDescription = "home_screen_bg",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
-                Text(
-                    "Home Screen",
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.padding(vertical = 20.dp)
+                CommunityHomeTopBar(
+                    onMenuBtnClicked = onMenuBtnClicked
+                )
+
+                CommunityFastTab(
+                    onStudyRecordItemClicked = { },
+                    onCollegeItemClicked = { },
+                    onMarketplaceClicked = { }
+                )
+
+                Spacer(modifier = Modifier.height(30.dp))
+                TrendingPostsSection()
+
+                Spacer(modifier = Modifier.height(30.dp))
+                HomeBulletinBoard(
+                    onBulletinBoardMoreClicked = { }
                 )
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun CommunityScreenPreview(modifier: Modifier = Modifier) {
+    CommunityScreen(
+        onMenuBtnClicked = { }
+    )
 }
