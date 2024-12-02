12ca075abc9c9a48dfc606669c7ba1c1d6ccf8b7
chrin05 <rinring105@gmail.com>
2024-12-02 17:30:17 +0900

    add/#14: dependency 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.kotlin.serialization)
     kotlin("kapt")
-
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
 }
 
 kapt {
