d29066b772762194f79a90961ca6b8971a29484c
chrin05 <rinring105@gmail.com>
2024-12-02 17:31:14 +0900

    feat#14: mainScreen 정의

--- app/src/main/java/com/example/togedy_android/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/togedy_android/presentation/main/MainActivity.kt
@@ -4,33 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.example.togedy_android.R
-import com.example.togedy_android.presentation.ui.bottom_nav.BottomNavigationItem
-import com.example.togedy_android.presentation.ui.model.NavGraph
-import com.example.togedy_android.ui.theme.Togedy_AndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,86 +12,74 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            Togedy_AndroidTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-//                    BottomNavigationBar()
-                    val navController = rememberNavController()
-                    val navBackStackEntry by navController.currentBackStackEntryAsState()
-                    val currentRoute = navBackStackEntry?.destination?.route
-                    Scaffold(
-                        modifier = Modifier.fillMaxSize(),
-                        bottomBar = {
-                            if (currentRoute !in listOf(
-                                    "launchPage",
-                                    "signIn",
-                                    "joinMembership",
-                                    "ResignMemberShip"
-                                )
-                            ) {
-                                NavigationBar(
-                                    containerColor = colorResource(id = R.color.white),
-                                    tonalElevation = 10.dp
-                                ) {
-                                    BottomNavigationItem().bottomNavigationItems()
-                                        .forEachIndexed { _, item ->
-                                            NavigationBarItem(
-                                                selected = item.route == currentRoute,
-                                                label = {
-                                                    Text(
-                                                        text = item.label,
-                                                        fontWeight = FontWeight.Medium,
-                                                        fontSize = 12.sp
-                                                    )
-                                                },
-                                                onClick = {
-                                                    navController.navigate(item.route) {
-                                                        popUpTo(navController.graph.findStartDestination().id) {
-                                                            saveState = true
-                                                        }
-                                                        launchSingleTop = true
-                                                        restoreState = true
-                                                    }
-                                                },
-                                                icon = {
-                                                    Icon(
-                                                        painterResource(id = item.icon),
-                                                        contentDescription = item.label
-                                                    )
-                                                },
-                                                colors = NavigationBarItemDefaults.colors(
-                                                    selectedIconColor = colorResource(id = R.color.yellow_main),
-                                                    unselectedIconColor = colorResource(id = R.color.gray400),
-                                                    selectedTextColor = colorResource(id = R.color.yellow_main),
-                                                    unselectedTextColor = colorResource(id = R.color.gray400),
-                                                    indicatorColor = Color.Transparent
-                                                )
-                                            )
-                                        }
-                                }
-                            }
-                        }
-                    ) {
-                        Box(
-                            modifier = Modifier.padding(it)
-                        ) {
-                            NavGraph(navController)
-                        }
-                    }
-                }
-            }
+            MainScreen()
+//            Togedy_AndroidTheme {
+//                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+////                    BottomNavigationBar()
+//                    val navController = rememberNavController()
+//                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+//                    val currentRoute = navBackStackEntry?.destination?.route
+//                    Scaffold(
+//                        modifier = Modifier.fillMaxSize(),
+//                        bottomBar = {
+//                            if (currentRoute !in listOf(
+//                                    "launchPage",
+//                                    "signIn",
+//                                    "joinMembership",
+//                                    "ResignMemberShip"
+//                                )
+//                            ) {
+//                                NavigationBar(
+//                                    containerColor = colorResource(id = R.color.white),
+//                                    tonalElevation = 10.dp
+//                                ) {
+//                                    BottomNavigationItem().bottomNavigationItems()
+//                                        .forEachIndexed { _, item ->
+//                                            NavigationBarItem(
+//                                                selected = item.route == currentRoute,
+//                                                label = {
+//                                                    Text(
+//                                                        text = item.label,
+//                                                        fontWeight = FontWeight.Medium,
+//                                                        fontSize = 12.sp
+//                                                    )
+//                                                },
+//                                                onClick = {
+//                                                    navController.navigate(item.route) {
+//                                                        popUpTo(navController.graph.findStartDestination().id) {
+//                                                            saveState = true
+//                                                        }
+//                                                        launchSingleTop = true
+//                                                        restoreState = true
+//                                                    }
+//                                                },
+//                                                icon = {
+//                                                    Icon(
+//                                                        painterResource(id = item.icon),
+//                                                        contentDescription = item.label
+//                                                    )
+//                                                },
+//                                                colors = NavigationBarItemDefaults.colors(
+//                                                    selectedIconColor = colorResource(id = R.color.yellow_main),
+//                                                    unselectedIconColor = colorResource(id = R.color.gray400),
+//                                                    selectedTextColor = colorResource(id = R.color.yellow_main),
+//                                                    unselectedTextColor = colorResource(id = R.color.gray400),
+//                                                    indicatorColor = Color.Transparent
+//                                                )
+//                                            )
+//                                        }
+//                                }
+//                            }
+//                        }
+//                    ) {
+//                        Box(
+//                            modifier = Modifier.padding(it)
+//                        ) {
+//                            NavGraph(navController)
+//                        }
+//                    }
+//                }
+//            }
         }
     }
 }
-
-//@Composable
-//fun Greeting() {
-//    BottomNavigationBar()
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    Togedy_AndroidTheme {
-//        Greeting()
-//    }
-//}
---
+++ app/src/main/java/com/example/togedy_android/presentation/main/MainScreen.kt
@@ -0,0 +1,58 @@
+package com.example.togedy_android.presentation.main
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.example.togedy_android.core.navigation.Route
+import com.example.togedy_android.presentation.main.component.MainBottomBars
+import com.example.togedy_android.presentation.ui.screens.community.navigation.communityScreen
+import com.example.togedy_android.presentation.ui.screens.gptscreen.navigation.gptScreen
+
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    mainNavigator: MainNavigator = rememberMainNavigator()
+) {
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        bottomBar = {
+            MainBottomBars(
+                tabs = MainTab.entries,
+                visibility = mainNavigator.showBottomBar(),
+                onTabSelect = mainNavigator::navigate,
+                selectedTab = mainNavigator.currentTab
+            )
+        }
+    ) { innerPadding ->
+        MainNavHost(
+            modifier = Modifier.padding(innerPadding),
+            navController = mainNavigator.navController,
+            startDestination = mainNavigator.startDestination
+        )
+    }
+}
+
+@Composable
+private fun MainNavHost(
+    navController: NavHostController,
+    startDestination: Route,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+    ) {
+        communityScreen(
+            navController = navController,
+            modifier = modifier
+        )
+        gptScreen(
+            navController = navController,
+            modifier = modifier
+        )
+    }
+}
