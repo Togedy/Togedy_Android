19fa4910a80eeb1d0599091fba06e71589a27df0
chrin05 <rinring105@gmail.com>
2024-12-02 17:36:21 +0900

    fix#14: import 추가

--- app/src/main/java/com/example/togedy_android/presentation/main/MainNavigator.kt
+++ app/src/main/java/com/example/togedy_android/presentation/main/MainNavigator.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
