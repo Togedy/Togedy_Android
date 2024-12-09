33c0c14566c1fca3a461e9f4cc7f8520a37a5d8b
TaejungKim <tae0803@naver.com>
2024-12-10 00:06:28 +0900

    feat/#20: community 상세 화면 response data 정의

---
+++ app/src/main/java/com/example/togedy_android/data/remote/model/response/CommunityDetailResponseDto.kt
@@ -0,0 +1,38 @@
+package com.example.togedy_android.data.remote.model.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CommunityDetailResponseDto (
+    @SerialName("title") val title: String,
+    @SerialName("createdAt") val createdAt: String,
+    @SerialName("content") val content: String,
+    @SerialName("postImages") val postImages: List<String>,
+    @SerialName("likeCount") val likeCount: Int,
+    @SerialName("commentCount") val commentCount: Int,
+    @SerialName("comments") val comments: List<DetailCommentsDto>,
+    @SerialName("postLike") val postLike: Boolean
+)
+
+@Serializable
+data class DetailCommentsDto(
+    @SerialName("commentId") val commentId: String,
+    @SerialName("userProfileImg") val userProfileImg: String?,
+    @SerialName("userName") val userName: String,
+    @SerialName("content") val content: String,
+    @SerialName("likeCount") val likeCount: Int,
+    @SerialName("replies") val replies: List<DetailRepliesDto>,
+    @SerialName("commentLike") val commentLike: Boolean
+)
+
+@Serializable
+data class DetailRepliesDto(
+    @SerialName("commentId") val commentId: String,
+    @SerialName("userProfileImg") val userProfileImg: String?,
+    @SerialName("userName") val userName: String,
+    @SerialName("content") val content: String,
+    @SerialName("likeCount") val likeCount: Int,
+    @SerialName("replies") val replies: List<String>,
+    @SerialName("commentLike") val commentLike: Boolean
+)
