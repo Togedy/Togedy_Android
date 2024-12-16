b5c2fb525be0fac4c7dfaa4823aa857f8a0a5dbc
chrin05 <rinring105@gmail.com>
2024-12-16 14:40:09 +0900

    chore/#34: model 정리

--- app/src/main/java/com/example/togedy_android/domain/model/planner/PlanItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.togedy_android.domain.model.planner
-
-data class PlanItem(
-    val todoID: Int,
-    val subjectColor: String,
-    val subjectId: Int = -3,
-    val title: String,
-    val status: String,
-)
--- app/src/main/java/com/example/togedy_android/domain/model/planner/StudyTagId.kt
+++ app/src/main/java/com/example/togedy_android/domain/model/planner/StudyTagId.kt
@@ -1,8 +1,5 @@
 package com.example.togedy_android.domain.model.planner
 
-import kotlinx.serialization.SerialName
-
 data class StudyTagId(
-    @SerialName("studyTagId")
     val studyTagId: Int,
 )
